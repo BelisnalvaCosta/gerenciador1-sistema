79dd67d292bb3c950d85e7678f007adbd439789e
BelisnalvaCosta <bella-costa@hotmail.com.br>
2025-01-23 17:05:53 -0300

    Project created

---
+++ src/main/java/com/proj_integrador3/gerenciador_sistema/GerenciadorSistemaApplication.java
@@ -0,0 +1,13 @@
+package com.proj_integrador3.gerenciador_sistema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorSistemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorSistemaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proj_integrador3/gerenciador_sistema/GerenciadorSistemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proj_integrador3.gerenciador_sistema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorSistemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
